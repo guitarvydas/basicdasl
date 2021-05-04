0e69fdb53b848de7d5e049ece13481a0b4257f8b
Paul Tarvydas <guitarvydas@gmail.com>
2021-05-03 23:39:45 -0400

    basic1 working

--- fb.pl
+++ fb.pl
@@ -2,33 +2,13 @@
 
 
 
->outputPort("B","subcomponent").
->outputPort("out","main").
-[]
-[contained("subcomponent","main").
-]
 component("main",nil).
-component("subcomponent",nil).
 connection(id0,"main").
-connection(id0,"subcomponent").
-connection(id3,"main").
-main<inputPort("in","main").
-pairComponent(id1,".").
+inputPort("in","main").
+outputPort("out","main").
 pairComponent(id1,".").
 pairComponent(id2,".").
-pairComponent(id2,"subcomponent").
-pairComponent(id4,"subcomponent").
-pairComponent(id5,".").
-pairPort(id1,"A").
 pairPort(id1,"in").
-pairPort(id2,"A").
-pairPort(id2,"B").
-pairPort(id4,"B").
-pairPort(id5,"out").
+pairPort(id2,"out").
 receiver(id2,id0).
-receiver(id2,id0).
-receiver(id5,id3).
 sender(id1,id0).
-sender(id1,id0).
-sender(id4,id3).
-subcomponent<inputPort("A","subcomponent").
