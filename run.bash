cc06feadd4ba20df45b888fba3d39530a7b41685
Paul Tarvydas <guitarvydas@gmail.com>
2021-05-03 23:29:24 -0400

    baseline

---
+++ run.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+target=basic2
+set -e
+echo >_.pl
+../grasem/run.bash dasl.grasem >_.js
+cat foreign.js _.js >_dasl.js
+node _dasl.js < $target.dasl >> _.pl
+
+awk -f name-mangling.awk < _.pl | sort > fb.pl
+cat fb.pl
+
+
