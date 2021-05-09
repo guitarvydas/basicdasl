cf3bbaa4a93b350f631b4d7d50bc4369ac8f2e4e
Paul Tarvydas <guitarvydas@gmail.com>
2021-05-09 11:37:12 -0400

    graphicalObjects & roadmap
    gobjects - circle, comp, rect, cyl
    
    
    missing rects on layers 2
    
    
    arrow objects wip
    
    
    replace leading &quot;
    
    
    replace trailing &quot;
    
    
    wip: arrows with list of receivers and commas
    
    
    wip: trim ()
    
    
    change unicode quotes to ascii quotes
    
    
    change unicode quotes in str()
    
    
    updates to roadmap

--- pseudo/fb.pl
+++ pseudo/fb.pl
@@ -1,130 +1,272 @@
 
 
-		<expansionState>0,1,52,54,57,60,63,74,76,87,89,100,102</expansionState>
-		<title>Diagrams</title>
-	</head>
-</body>
-</opml>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-</outline>
-<?xml version="1.0" encoding="UTF-8" ?>
-<body>
-<head>
-<opml version="1.0">
-<outlinetext="(null)"/>
-<outlinetext="a - r"/>
-<outlinetext="a -&gt; u"/>
-<outlinetext="arrows"><outlinetext="c - k"/>
-<outlinetext="arrows"><outlinetext="c -&gt; m"/>
-<outlinetext="b - l, o"/>
-<outlinetext="b -&gt; p"/>
-<outlinetext="circle &quot;d&quot;"><outlinetext="color=yellow"/>
-<outlinetext="circle “a&quot;"><outlinetext="color=green"/>
-<outlinetext="circle “a”"><outlinetext="color=green"/>
-<outlinetext="circle “b&quot;"><outlinetext="color=green"/>
-<outlinetext="circle “b&quot;"><outlinetext="color=green"/>
-<outlinetext="circle “b”"><outlinetext="color=green"/>
-<outlinetext="circle “c”"><outlinetext="color=green"/>
-<outlinetext="circle “c”"><outlinetext="color=green"/>
-<outlinetext="circle “d&quot;"><outlinetext="color=yellow"/>
-<outlinetext="circle “g”"><outlinetext="color=yellow"/>
-<outlinetext="comp &quot;top level”"><outlinetext="circle “a”"><outlinetext="color=green"/>
-<outlinetext="comp “c” ”∀ children of my Composite Template&quot;"><outlinetext="text=”∀ children of my Composite Template&quot;"/>
-<outlinetext="comp “e” “∀ connections of my Composite Template”"><outlinetext="text=“∀ connections of my Composite Template”"/>
-<outlinetext="comp “e”"><outlinetext="text=&quot;clone connection”"/>
-<outlinetext="comp “e”"><outlinetext="text=“make instance”"/>
-<outlinetext="comp “g&quot;"><outlinetext="text=“fixup connection”"/>
-<outlinetext="comp “g”"><outlinetext="text=&quot;invent name”"/>
-<outlinetext="comp “i&quot;"><outlinetext="text=&quot;insert connection into runnable”"/>
-<outlinetext="comp “i&quot;"><outlinetext="text=“recursively instantiate”"/>
-<outlinetext="comp “k&quot;"><outlinetext="text=&quot;insert child into children of my runnable”"/>
-<outlinetext="cyl “d”"/>
-<outlinetext="cyl “f”"/>
-<outlinetext="cyl “f”"><outlinetext="text=“child instance”"/>
-<outlinetext="cyl “f”"><outlinetext="text=“runnable connection with holes”"/>
-<outlinetext="cyl “h&quot;"><outlinetext="text=&quot;named child instance”"/>
-<outlinetext="cyl “h&quot;"><outlinetext="text=“fixed up connection”"/>
-<outlinetext="cyl “j&quot;"><outlinetext="text=“filled child instance”"/>
-<outlinetext="cyl “j”"><outlinetext="text=“final runnable”"/>
-<outlinetext="cyl “l&quot;"><outlinetext="text=“my runnable filled in with children”"/>
-<outlinetext="f - n"/>
-<outlinetext="f -&gt; o"/>
-<outlinetext="g -&gt; h"/>
-<outlinetext="h - g"/>
-<outlinetext="h -&gt; r"/>
-<outlinetext="j -&gt; t"/>
-<outlinetext="l - d"/>
-<outlinetext="l -&gt; d"/>
-<outlinetext="m - f"/>
-<outlinetext="n -&gt; f"/>
-<outlinetext="p - h"/>
-<outlinetext="port “a”"><outlinetext="color=green"/>
-<outlinetext="port “a”"><outlinetext="color=green"/>
-<outlinetext="port “a”"><outlinetext="color=green"/>
-<outlinetext="port “b”"><outlinetext="color=green"/>
-<outlinetext="port “b”"><outlinetext="color=green"/>
-<outlinetext="port “b”"><outlinetext="color=green"/>
-<outlinetext="port “c”"><outlinetext="color=yellow"/>
-<outlinetext="port “c”"><outlinetext="color=yellow"/>
-<outlinetext="port “c”"><outlinetext="color=yellow"/>
-<outlinetext="s - l"/>
-<outlinetext="s -&gt; j"/>
-<outlinetext="stroke-width=1"/>
-<outlinetext="stroke-width=1"/>
-<outlinetext="stroke-width=1"/>
-<outlinetext="stroke-width=1"/>
-<outlinetext="stroke-width=1"/>
-<outlinetext="stroke-width=1"/>
-<outlinetext="stroke-width=1"/>
-<outlinetext="stroke-width=1"/>
-<outlinetext="stroke-width=1"/>
-<outlinetext="stroke-width=1"/>
-<outlinetext="stroke-width=1"/>
-<outlinetext="stroke-width=1"/>
-<outlinetext="stroke-width=1"/>
-<outlinetext="stroke-width=1"/>
-<outlinetext="stroke-width=1"/>
-<outlinetext="stroke-width=3"/>
-<outlinetext="text=“child”"/>
-<outlinetext="text=“my Composite Template”"/>
-<outlinetext="text=“my Composite Template”"/>
-<outlinetext="text=“my runnable”"/>
-<outlinetext="text=“my runnable”"/>
-<outlinetext="v -&gt; l"/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+arrowBegin(id40, a).
+arrowBegin(id40, b).
+arrowBegin(id40, c).
+arrowBegin(id40, f).
+arrowBegin(id40, g).
+arrowBegin(id40, h).
+arrowBegin(id40, j).
+arrowBegin(id40, l).
+arrowBegin(id40, n).
+arrowBegin(id40, s).
+arrowBegin(id40, v).
+arrowBegin(id69, a).
+arrowBegin(id69, b).
+arrowBegin(id69, c).
+arrowBegin(id69, f).
+arrowBegin(id69, h).
+arrowBegin(id69, l).
+arrowBegin(id69, m).
+arrowBegin(id69, p).
+arrowBegin(id69, s).
+arrowEnd(id40, [d]).
+arrowEnd(id40, [f]).
+arrowEnd(id40, [h]).
+arrowEnd(id40, [j]).
+arrowEnd(id40, [l]).
+arrowEnd(id40, [m]).
+arrowEnd(id40, [o]).
+arrowEnd(id40, [p]).
+arrowEnd(id40, [r]).
+arrowEnd(id40, [t]).
+arrowEnd(id40, [u]).
+arrowEnd(id69, [d]).
+arrowEnd(id69, [f]).
+arrowEnd(id69, [g]).
+arrowEnd(id69, [h]).
+arrowEnd(id69, [k]).
+arrowEnd(id69, [l,o]).
+arrowEnd(id69, [l]).
+arrowEnd(id69, [n]).
+arrowEnd(id69, [r]).
+circle(id1, "a").
+circle(id11, "c").
+circle(id13, "d").
+circle(id3, "b").
+circle(id45, "a").
+circle(id46, "b").
+circle(id47, "c").
+circle(id7, "a").
+circle(id70, "d").
+circle(id73, "g").
+circle(id9, "b").
+color(id1, green).
+color(id11, green).
+color(id13, yellow).
+color(id16, green).
+color(id17, yellow).
+color(id22, green).
+color(id23, green).
+color(id24, yellow).
+color(id29, green).
+color(id3, green).
+color(id30, yellow).
+color(id35, green).
+color(id36, green).
+color(id37, yellow).
+color(id45, green).
+color(id46, green).
+color(id47, green).
+color(id50, green).
+color(id51, green).
+color(id52, yellow).
+color(id57, green).
+color(id58, green).
+color(id59, yellow).
+color(id64, green).
+color(id65, green).
+color(id66, yellow).
+color(id7, green).
+color(id70, yellow).
+color(id73, yellow).
+color(id9, green).
+comp(id0, "A").
+comp(id14, "e").
+comp(id20, "g").
+comp(id27, "i").
+comp(id33, "k").
+comp(id43, "e").
+comp(id48, "e").
+comp(id5, "c").
+comp(id55, "g").
+comp(id62, "i").
+cylinder(id18, "f").
+cylinder(id25, "h").
+cylinder(id31, "j").
+cylinder(id38, "l").
+cylinder(id41, "d").
+cylinder(id53, "f").
+cylinder(id60, "h").
+cylinder(id67, "j").
+cylinder(id71, "f").
+rect(id16, "m").
+rect(id17, "n").
+rect(id22, "o").
+rect(id23, "p").
+rect(id24, "q").
+rect(id29, "r").
+rect(id30, "s").
+rect(id35, "t").
+rect(id36, "u").
+rect(id37, "v").
+rect(id50, "k").
+rect(id51, "l").
+rect(id52, "m").
+rect(id57, "n").
+rect(id58, "o").
+rect(id59, "p").
+rect(id64, "q").
+rect(id65, "r").
+rect(id66, "s").
+str(tid10, "my Composite Template").
+str(tid12, "child").
+str(tid15, "make instance").
+str(tid19, "child instance").
+str(tid2, "my Composite Template").
+str(tid21, "invent name").
+str(tid26, "named child instance").
+str(tid28, "recursively instantiate").
+str(tid32, "filled child instance").
+str(tid34, "insert child into children of my runnable").
+str(tid39, "my runnable filled in with children").
+str(tid4, "my runnable").
+str(tid42, "my runnable filled in with children").
+str(tid44, "∀ connections of my Composite Template").
+str(tid49, "clone connection").
+str(tid54, "runnable connection with holes").
+str(tid56, "fixup connection").
+str(tid6, "∀ children of my Composite Template").
+str(tid61, "fixed up connection").
+str(tid63, "insert connection into runnable").
+str(tid68, "final runnable").
+str(tid72, "runnable").
+str(tid8, "my runnable").
+strokeWidth(id1, 3).
+strokeWidth(id3, 1).
+strokeWidth(id45, 1).
+strokeWidth(id46, 1).
+strokeWidth(id47, 1).
+strokeWidth(id50, 1).
+strokeWidth(id51, 1).
+strokeWidth(id52, 1).
+strokeWidth(id57, 1).
+strokeWidth(id58, 1).
+strokeWidth(id59, 1).
+strokeWidth(id64, 1).
+strokeWidth(id65, 1).
+strokeWidth(id66, 1).
+strokeWidth(id70, 1).
+strokeWidth(id73, 1).
+text(id1, tid2).
+text(id11, tid12).
+text(id14, tid15).
+text(id18, tid19).
+text(id20, tid21).
+text(id25, tid26).
+text(id27, tid28).
+text(id3, tid4).
+text(id31, tid32).
+text(id33, tid34).
+text(id38, tid39).
+text(id41, tid42).
+text(id43, tid44).
+text(id48, tid49).
+text(id5, tid6).
+text(id53, tid54).
+text(id55, tid56).
+text(id60, tid61).
+text(id62, tid63).
+text(id67, tid68).
+text(id7, tid8).
+text(id71, tid72).
+text(id9, tid10).
