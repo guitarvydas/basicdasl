fdb53450bd57f689d89546f54f9983ac851e04d6
Paul Tarvydas <guitarvydas@gmail.com>
2021-05-10 12:16:15 -0400

    fix arrow bug by creating a new arrow

--- pseudo/fb.pl
+++ pseudo/fb.pl
@@ -89,57 +89,77 @@
 
 
 
-arrowBegin(id40, c_a).
-arrowBegin(id40, c_b).
-arrowBegin(id40, c_c).
-arrowBegin(id40, c_e_n).
-arrowBegin(id40, c_f).
-arrowBegin(id40, c_g_q).
-arrowBegin(id40, c_h).
-arrowBegin(id40, c_i_s).
-arrowBegin(id40, c_j).
-arrowBegin(id40, c_k_v).
-arrowBegin(id40, c_l).
-arrowBegin(id69, c_a).
-arrowBegin(id69, c_b).
-arrowBegin(id69, c_c).
-arrowBegin(id69, c_e_m).
-arrowBegin(id69, c_f).
-arrowBegin(id69, c_g_p).
-arrowBegin(id69, c_h).
-arrowBegin(id69, c_i_s).
-arrowBegin(id69, c_j).
-arrowEnd(id40, [c_d]).
-arrowEnd(id40, [c_e_m]).
-arrowEnd(id40, [c_f]).
-arrowEnd(id40, [c_g_o]).
-arrowEnd(id40, [c_g_p]).
-arrowEnd(id40, [c_h]).
-arrowEnd(id40, [c_i_r]).
-arrowEnd(id40, [c_j]).
-arrowEnd(id40, [c_k_t]).
-arrowEnd(id40, [c_k_u]).
-arrowEnd(id40, [c_l]).
-arrowEnd(id69, [c_d]).
-arrowEnd(id69, [c_e_k]).
-arrowEnd(id69, [c_e_l,c_g_o]).
-arrowEnd(id69, [c_f]).
-arrowEnd(id69, [c_g_n]).
-arrowEnd(id69, [c_h]).
-arrowEnd(id69, [c_i_q]).
-arrowEnd(id69, [c_i_r]).
-arrowEnd(id69, [j]).
+arrow(id40, a41).
+arrow(id40, a42).
+arrow(id40, a43).
+arrow(id40, a44).
+arrow(id40, a45).
+arrow(id40, a46).
+arrow(id40, a47).
+arrow(id40, a48).
+arrow(id40, a49).
+arrow(id40, a50).
+arrow(id40, a51).
+arrow(id80, a81).
+arrow(id80, a82).
+arrow(id80, a83).
+arrow(id80, a84).
+arrow(id80, a85).
+arrow(id80, a86).
+arrow(id80, a87).
+arrow(id80, a88).
+arrow(id80, a89).
+arrowBegin(a41, c_c).
+arrowBegin(a42, c_e_n).
+arrowBegin(a43, c_f).
+arrowBegin(a44, c_b).
+arrowBegin(a45, c_g_q).
+arrowBegin(a46, c_h).
+arrowBegin(a47, c_i_s).
+arrowBegin(a48, c_j).
+arrowBegin(a49, c_a).
+arrowBegin(a50, c_k_v).
+arrowBegin(a51, c_l).
+arrowBegin(a81, c_c).
+arrowBegin(a82, c_b).
+arrowBegin(a83, c_e_m).
+arrowBegin(a84, c_f).
+arrowBegin(a85, c_g_p).
+arrowBegin(a86, c_h).
+arrowBegin(a87, c_a).
+arrowBegin(a88, c_i_s).
+arrowBegin(a89, c_j).
+arrowEnd(a41, [c_e_m]).
+arrowEnd(a42, [c_f]).
+arrowEnd(a43, [c_g_o]).
+arrowEnd(a44, [c_g_p]).
+arrowEnd(a45, [c_h]).
+arrowEnd(a46, [c_i_r]).
+arrowEnd(a47, [c_j]).
+arrowEnd(a48, [c_k_t]).
+arrowEnd(a49, [c_k_u]).
+arrowEnd(a50, [c_l]).
+arrowEnd(a51, [c_d]).
+arrowEnd(a81, [c_e_k]).
+arrowEnd(a82, [c_e_l,c_g_o]).
+arrowEnd(a83, [c_f]).
+arrowEnd(a84, [c_g_n]).
+arrowEnd(a85, [c_h]).
+arrowEnd(a86, [c_i_q]).
+arrowEnd(a87, [c_i_r]).
+arrowEnd(a88, [j]).
+arrowEnd(a89, [c_d]).
 circle(id1, a).
 circle(id11, c_c).
 circle(id13, c_d).
 circle(id3, b).
-circle(id45, e_a).
-circle(id46, e_b).
-circle(id47, e_c).
+circle(id56, e_a).
+circle(id57, e_b).
+circle(id58, e_c).
 circle(id7, c_a).
-circle(id70, d).
-circle(id73, g).
 circle(id9, c_b).
+circle(id90, d).
+circle(id93, g).
 color(id1, green).
 color(id11, green).
 color(id13, yellow).
@@ -154,22 +174,22 @@ color(id30, yellow).
 color(id35, green).
 color(id36, green).
 color(id37, yellow).
-color(id45, green).
-color(id46, green).
-color(id47, green).
-color(id50, green).
-color(id51, green).
-color(id52, yellow).
+color(id56, green).
 color(id57, green).
 color(id58, green).
-color(id59, yellow).
-color(id64, green).
-color(id65, green).
-color(id66, yellow).
+color(id61, green).
+color(id62, green).
+color(id63, yellow).
+color(id68, green).
+color(id69, green).
 color(id7, green).
 color(id70, yellow).
-color(id73, yellow).
+color(id75, green).
+color(id76, green).
+color(id77, yellow).
 color(id9, green).
+color(id90, yellow).
+color(id93, yellow).
 comp(id0, z).
 comp(id14, c_e).
 comp(id16, c_e_m).
@@ -189,25 +209,25 @@ comp(id35, c_k_t).
 comp(id36, c_k_u).
 comp(id37, c_h_v).
 comp(id38, c_l).
-comp(id41, d).
-comp(id43, e).
-comp(id48, e_e).
 comp(id5, c).
-comp(id50, e_e_k).
-comp(id51, e_e_l).
-comp(id52, e_e_m).
-comp(id53, e_f).
-comp(id55, e_g).
-comp(id57, e_g_n).
-comp(id58, e_g_o).
-comp(id59, e_g_p).
-comp(id60, e_h).
-comp(id62, e_i).
-comp(id64, e_i_q).
-comp(id65, e_i_r).
-comp(id66, e_i_s).
-comp(id67, e_j).
-comp(id71, f).
+comp(id52, d).
+comp(id54, e).
+comp(id59, e_e).
+comp(id61, e_e_k).
+comp(id62, e_e_l).
+comp(id63, e_e_m).
+comp(id64, e_f).
+comp(id66, e_g).
+comp(id68, e_g_n).
+comp(id69, e_g_o).
+comp(id70, e_g_p).
+comp(id71, e_h).
+comp(id73, e_i).
+comp(id75, e_i_q).
+comp(id76, e_i_r).
+comp(id77, e_i_s).
+comp(id78, e_j).
+comp(id91, f).
 str(tid10, "my Composite Template").
 str(tid12, "child").
 str(tid15, "make instance").
@@ -220,33 +240,33 @@ str(tid32, "filled child instance").
 str(tid34, "insert child into children of my runnable").
 str(tid39, "my runnable filled in with children").
 str(tid4, "my runnable").
-str(tid42, "my runnable filled in with children").
-str(tid44, "∀ connections of my Composite Template").
-str(tid49, "clone connection").
-str(tid54, "runnable connection with holes").
-str(tid56, "fixup connection").
+str(tid53, "my runnable filled in with children").
+str(tid55, "∀ connections of my Composite Template").
 str(tid6, "∀ children of my Composite Template").
-str(tid61, "fixed up connection").
-str(tid63, "insert connection into runnable").
-str(tid68, "final runnable").
-str(tid72, "runnable").
+str(tid60, "clone connection").
+str(tid65, "runnable connection with holes").
+str(tid67, "fixup connection").
+str(tid72, "fixed up connection").
+str(tid74, "insert connection into runnable").
+str(tid79, "final runnable").
 str(tid8, "my runnable").
+str(tid92, "runnable").
 strokeWidth(id1, 3).
 strokeWidth(id3, 1).
-strokeWidth(id45, 1).
-strokeWidth(id46, 1).
-strokeWidth(id47, 1).
-strokeWidth(id50, 1).
-strokeWidth(id51, 1).
-strokeWidth(id52, 1).
+strokeWidth(id56, 1).
 strokeWidth(id57, 1).
 strokeWidth(id58, 1).
-strokeWidth(id59, 1).
-strokeWidth(id64, 1).
-strokeWidth(id65, 1).
-strokeWidth(id66, 1).
+strokeWidth(id61, 1).
+strokeWidth(id62, 1).
+strokeWidth(id63, 1).
+strokeWidth(id68, 1).
+strokeWidth(id69, 1).
 strokeWidth(id70, 1).
-strokeWidth(id73, 1).
+strokeWidth(id75, 1).
+strokeWidth(id76, 1).
+strokeWidth(id77, 1).
+strokeWidth(id90, 1).
+strokeWidth(id93, 1).
 text(id1, tid2).
 text(id11, tid12).
 text(id14, tid15).
@@ -258,15 +278,15 @@ text(id3, tid4).
 text(id31, tid32).
 text(id33, tid34).
 text(id38, tid39).
-text(id41, tid42).
-text(id43, tid44).
-text(id48, tid49).
 text(id5, tid6).
-text(id53, tid54).
-text(id55, tid56).
-text(id60, tid61).
-text(id62, tid63).
-text(id67, tid68).
+text(id52, tid53).
+text(id54, tid55).
+text(id59, tid60).
+text(id64, tid65).
+text(id66, tid67).
 text(id7, tid8).
 text(id71, tid72).
+text(id73, tid74).
+text(id78, tid79).
 text(id9, tid10).
+text(id91, tid92).
