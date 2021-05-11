3db25876f55e8d040a25ce6add692f76038b29f7
Paul Tarvydas <guitarvydas@gmail.com>
2021-05-11 00:09:12 -0400

    wip: types grammar
    bagof instead of forall ; run.bash
    
    
    types grammar

--- pseudo/run.bash
+++ pseudo/run.bash
@@ -5,16 +5,16 @@ echo >_.pl
 cat foreign.js _.js >_opml2fb.js
 node _opml2fb.js < diagrams.opml >> _.pl
 
-sort _.pl > fb.pl
-#cat fb.pl
+sort _.pl | grep -v '^$' > fb.pl
+
 swipl -q \
       -g 'consult(fb).' \
       -g 'consult(q).' \
-      -g 'printAllABegin.' \
-      -g 'halt.'
-# I don't understand: if we try to printAllABegin and printAllAEnd in the same script, we get only the first set of output
-swipl -q \
-      -g 'consult(fb).' \
-      -g 'consult(q).' \
-      -g 'printAllAEnd.' \
-      -g 'halt.'
+      -g 'printAllABegin(_).' \
+      -g 'printAllAEnd(_).' \
+      -g 'halt.' \
+      > _2.pl
+
+sort _2.pl >_3.pl
+cat fb.pl _3.pl > _.pl
+cp _.pl fb.pl
