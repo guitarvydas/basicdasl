c55cce5a645bc06e5194695d78c6739e7ef01cdf
Paul Tarvydas <guitarvydas@gmail.com>
2021-05-16 10:30:41 -0400

    port queries (and parent round-trip queries)
    port queries & parent round-trip queries

--- pseudo/dev.bash
+++ pseudo/dev.bash
@@ -1,11 +1,20 @@
 #!/bin/bash
 set -e
 echo >_.pl
-../../grasem/run.bash opml2fb.grasem >_.js
+#../../grasem/run.bash opml2fb.grasem >_.js
 cat foreign.js _.js >_opml2fb.js
-node _opml2fb.js < test.opml >> _.pl
+node _opml2fb.js < diagrams.opml >> _.pl
 
-sort _.pl > fb.pl
-#cat fb.pl
-cat _.pl
+sort _.pl | grep -v '^$' > fb.pl
 
+swipl -q \
+      -g 'consult(fb).' \
+      -g 'consult(q).' \
+      -g 'printAllABegin(_).' \
+      -g 'printAllAEnd(_).' \
+      -g 'halt.' \
+      > _2.pl
+
+sort _2.pl >_3.pl
+cat fb.pl _3.pl > _.pl
+cp _.pl fb.pl
--- pseudo/q.pl
+++ pseudo/q.pl
@@ -45,3 +45,67 @@ printAllAEnd(Bag) :-
     bagof(AID,printOneAEnd(AID),Bag).
 
 printAll :- printAllABegin, printAllAEnd.
+
+
+
+parent(P,C) :-
+    contains(PID,CID),
+    synonym(PID,P),
+    synonym(CID,C).
+
+display(P):-
+    setof(C,parent(P,C),Bag),
+    format("~w: ~w~n", [P,Bag]).
+
+displayAllPC:-
+    setof(P,display(P),_).
+
+
+
+externalPort(C) :-
+    circle(C,_).
+internalPort(R) :-
+    rect(R,_),
+    \+ comp(R,_).
+port(P):-
+    externalPort(P).
+port(P):-
+    internalPort(P).
+inputPort(P) :-
+    port(P).
+outputPort(P):-
+    port(P),
+    \+ inputPort(P).
+implicitPort(P):-
+    port(P),
+    strokeWidth(P,3).
+explicitPort(P):-
+    port(P),
+    \+ implicitPort(P).
+describePort(P):-
+    port(P),
+    format("port(~w,nil).~n", [P]),
+    describeLocation(P),
+    describeDirection(P),
+    describeConnectionType(P).
+describeLocation(P) :-
+    externalPort(P),
+    format("location(~w,external).~n", P).
+describeLocation(P) :-
+    internalPort(P),
+    format("location(~w,internal).~n", P).
+describeDirection(P) :-
+    inputPort(P),
+    format("direction(~w,input).~n", P).
+describeDirection(P) :-
+    outputPort(P),
+    format("direction(~w,output).~n", P).
+describeConnectionType(P) :-
+    explicitPort(P),
+    format("connectionType(~w,explicit).~n", P).
+describeConnectionType(P) :-
+    implicitPort(P),
+    format("connectionType(~w,implicit).~n", P).
+describeAllPorts:-
+    setof(P,describePort(P),_).
+
