7d26a00b30342c0b9398c5fe1e9f0b603e31ce76
Paul Tarvydas <guitarvydas@gmail.com>
2021-05-17 10:52:07 -0400

    wip: adding aka

--- pseudo/dev.bash
+++ pseudo/dev.bash
@@ -6,9 +6,10 @@ echo >_.pl
 ../../grasem/run.bash opml2fb.grasem >_.js
 cat foreign.js _.js >_opml2fb.js
 node _opml2fb.js < engine.opml >> _.pl
+sort _.pl | grep -v '^$' > engine_fb.pl
 
 swipl -q \
-      -g 'consult(fb).' \
+      -g 'consult(engine_fb).' \
       -g 'consult(q).' \
       -g 'describeAllPorts.' \
       -g 'printAllABegin(_).' \
@@ -17,5 +18,5 @@ swipl -q \
       > _4.pl
 
 sort _4.pl >_5.pl
-cat fb.pl _5.pl > _.pl
-cp _.pl engine-fb.pl
+cat engine_fb.pl _5.pl > _.pl
+cp _.pl engine_fb.pl
---
+++ pseudo/engine_fb.pl
@@ -0,0 +1,86 @@
+circle(id2, v_a).
+circle(id20, v_d).
+color(id11, green).
+color(id12, green).
+color(id13, yellow).
+color(id14, yellow).
+color(id17, green).
+color(id18, yellow).
+color(id2, green).
+color(id20, yellow).
+color(id4, green).
+color(id5, yellow).
+color(id8, green).
+color(id9, green).
+comp(id10, v_x_y_z).
+comp(id3, v_b).
+comp(id7, v_x_y).
+contains(id0, id1).
+contains(id1, id2).
+contains(id1, id20).
+contains(id1, id21).
+contains(id1, id3).
+contains(id1, id6).
+contains(id10, id11).
+contains(id10, id12).
+contains(id10, id13).
+contains(id16, id17).
+contains(id16, id18).
+contains(id3, id4).
+contains(id3, id5).
+contains(id6, id16).
+contains(id6, id19).
+contains(id6, id7).
+contains(id7, id10).
+contains(id7, id14).
+contains(id7, id15).
+contains(id7, id8).
+contains(id7, id9).
+cyl(id6, v_x_c).
+rect(id11, v_x_y_z_a).
+rect(id12, v_x_y_z_b).
+rect(id13, v_x_y_z_c).
+rect(id14, v_x_y_c).
+rect(id16, v_x_d).
+rect(id17, v_x_d_a).
+rect(id18, v_x_d_b).
+rect(id6, v_x_a).
+rect(id6, v_x_b).
+rect(id6, v_x_e).
+rect(id8, v_x_y_a).
+rect(id9, v_x_y_b).
+shape(id10, "component").
+shape(id11, "rect").
+shape(id12, "rect").
+shape(id13, "rect").
+shape(id14, "rect").
+shape(id16, "rect").
+shape(id17, "rect").
+shape(id18, "rect").
+shape(id2, "circle").
+shape(id20, "circle").
+shape(id3, "component").
+shape(id6, "cylinder").
+shape(id6, "rect").
+shape(id6, "rect").
+shape(id6, "rect").
+shape(id7, "component").
+shape(id8, "rect").
+shape(id9, "rect").
+text=comp v/x
+text=cyl "v/c&quot;
+text=line v/a v/b/a v/x/b
+text=line v/b/b/ v/c
+text=line v/c v/x/b
+text=line v/x/a v/x/y/a
+text=line v/x/b v/d
+text=line v/x/b/ v/x/y/b
+text=line v/x/c v/x/d/a/
+text=line v/x/d/b/ v/x/e
+text=line v/x/y/a z/y/z/a
+text=line v/x/y/b v/x/y/z/b
+text=line v/x/y/c v/x/c
+text=line v/x/y/z/c/ v/x/y/c
+text=rect v/b/a
+text=rect v/b/b
+text=v
