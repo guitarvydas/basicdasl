09d71727c8f6f4321ffaa204470a23cd355b8dc5
Paul Tarvydas <guitarvydas@gmail.com>
2021-05-21 17:42:49 -0400

    alpha: rewrite opml2fb.grasem
    wip: before lower-casing all rules
    
    
    containers and links
    
    
    links have attrs and rects ; saved in test.ohm and opml2fb.ohm
    
    
    resave test.opml

--- pseudo/foreign.js
+++ pseudo/foreign.js
@@ -19,3 +19,66 @@ function aend (s) {
     return `[${changeUnicodeQuotes(s.trim ().replace (" ", ","))}]`;
 }
 
+
+function incDepth () {
+    var i = scopeGet ("depth");
+    scopeModify ("depth", i + 1);
+    return i + 1;
+}
+function decDepth () {
+    var i = scopeGet ("depth");
+    scopeModify ("depth", i - 1);
+    return i - 1;
+}
+
+function tab (depth) {
+    let i = depth;
+    while (i > 0) {
+	process.stderr.write ('  ');
+	i -= 1;
+    }
+}
+
+function resetCounter () {
+    scopeAdd ('counter', 0);
+}
+
+function resetLineNumber () {
+    scopeAdd ('line', 0);
+}
+
+function incLineNumber () {
+    var n = scopeGet ('line');
+    scopeModify ('line', n + 1);
+}
+
+function sideEffectNewObject () {
+    scopeAdd ('Object', "id" + gen ());
+    return "";
+}
+
+function getObject () {
+    return scopeGet ('object');
+}
+
+function newTextObject () {
+    scopeAdd('textobject', "tid" + gen ());
+}
+
+function getTextObject () {
+    return scopeGet('textobject');
+}
+
+function newContainer () {
+    var container = getObject ();
+    scopeAdd ('container', container);
+    newObject ();
+}    
+
+function newArrow () {
+    scopeAdd ("arrow", "a" + gen ());
+}
+
+function getArrow () {
+    return scopeGet ('arrow');
+}
